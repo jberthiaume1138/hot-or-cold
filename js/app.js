9e130bfd28b4a3861069a45e09bee2c01003678b
Joseph Berthiaume <joenyc23@gmail.com>
2016-03-29 02:47:14 -0400

    event handers

--- js/app.js
+++ js/app.js
@@ -14,4 +14,43 @@ $(document).ready(function(){
 
 });
 
+//create event handler for NEW
+var ham = document.getElementsByClassName("new");
+var stuff = ham[0];
+console.log(stuff);
+stuff.addEventListener("click",function(event) {
+	// handler for li a class=new
+	console.log('new clicked');
+	newGame();
+});
 
+document.getElementById('guessButton').addEventListener("click",function(event) {
+// handler for <input type="submit" id="guessButton" class="button" name="submit" value="Guess">
+	console.log('guess clicked');
+	checkGuess();
+});
+
+
+// <input type="text" name="userGuess" id="userGuess" class="text" maxlength="3" autocomplete="off" placeholder="Enter your Guess" required="">
+
+
+// p span id=count // count of guesses
+
+
+function newGame() {
+	// reset the guess count/list
+	// this must be called on load as well
+	// call create number
+};
+
+function createNumber() {
+	// generate random number 1 - 100
+};
+
+function checkGuess () {
+	// grab user input from box, compare to createdNumber
+};
+
+function success() {
+
+};
