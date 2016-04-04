ee1317c1fa207f7edc7c1effd8c2c8445a5d1e70
jberthiaume1138 <joenyc23@gmail.com>
2016-04-04 18:58:47 -0400

    working on logic

--- js/app.js
+++ js/app.js
@@ -16,23 +16,28 @@ $(document).ready(function(){
 
 document.onload = newGame();
 
-//create event handler for NEW
-var ham = document.getElementsByClassName("new");
-var stuff = ham[0];
-console.log(stuff);
-stuff.addEventListener("click",function(event) {
-	// handler for li a class=new
-	console.log('new clicked');
-	newGame();
 
 
-});
 
-document.getElementById('guessButton').addEventListener("click",function(event) {
-// handler for <input type="submit" id="guessButton" class="button" name="submit" value="Guess">
-	console.log('guess clicked');
-	checkGuess();
-});
+function game() {
+	//create event handler for NEW
+	var ham = document.getElementsByClassName("new");
+	var stuff = ham[0];
+	console.log(stuff);
+	stuff.addEventListener("click",function(event) {
+		// handler for li a class=new
+		console.log('new clicked');
+		newGame();
+	});
+
+	document.getElementById('guessButton').addEventListener("click",function(event) {
+		// handler for <input type="submit" id="guessButton" class="button" name="submit" value="Guess">
+		console.log('guess button clicked');
+		checkGuess();
+	});
+
+};
+
 
 
 // <input type="text" name="userGuess" id="userGuess" class="text" maxlength="3" autocomplete="off" placeholder="Enter your Guess" required="">
@@ -40,7 +45,7 @@ document.getElementById('guessButton').addEventListener("click",function(event) 
 
 // p span id=count // count of guesses
 
-
+// function to generate a new random number and clear the guess list 
 function newGame() {
 	// reset the guess count/list
 	// this must be called on load as well
@@ -48,18 +53,33 @@ function newGame() {
 
 	// call create number
 
-	var randomnumber=Math.floor(Math.random()*101);
+	var randomnumber = Math.floor(Math.random()*101);
 	console.log(randomnumber);
 };
 
-function createNumber() {
-	// generate random number 1 - 100
-	// var randomnumber=Math.floor(Math.random()*11);
-	// console.log(randomnumber);
-};
+// function createNumber() {
+// 	// generate random number 1 - 100
+// 	// var randomnumber=Math.floor(Math.random()*11);
+// 	// console.log(randomnumber);
+// };
 
-function checkGuess () {
+function checkGuess (secretNumber) {
 	// grab user input from box, compare to createdNumber
+	var guess = document.getElementById('userGuess').value;
+	var delta = Math.abs(guess - secretNumber);
+
+	if (secretNumber = guess) {
+		console.log('success');
+		success();
+	}
+	else if (delta < 10) {
+		console.log('red hot');
+	}
+	else if (10 < delta < 20 ) {
+		console.log('hot')
+	}
+
+
 };
 
 function success() {
