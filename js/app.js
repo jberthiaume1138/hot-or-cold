aa1deaa0f224a92a3ff982f221ff6e259626db28
Joseph Berthiaume <joenyc23@gmail.com>
2016-04-05 01:10:27 -0400

    more logic

--- js/app.js
+++ js/app.js
@@ -16,8 +16,10 @@ $(document).ready(function(){
 
 document.onload = newGame();
 
-//create event handler for NEW
-var ham = document.getElementsByClassName("new");	// make all this more elegant
+// --------- create event handler for the NEW button ------------
+// could more simply do this with jQuery like the repo did with .WHAT
+// TODO: find a more elegant way to do this with vanilla Javascript
+var ham = document.getElementsByClassName("new");
 var stuff = ham[0];
 console.log(stuff);
 stuff.addEventListener("click",function(event) {
@@ -26,6 +28,7 @@ stuff.addEventListener("click",function(event) {
 	newGame();
 });
 
+// ---------- event handler for the guessButton -----------------
 document.getElementById('guessButton').addEventListener("click",function(event) {
 // handler for <input type="submit" id="guessButton" class="button" name="submit" value="Guess">
 	console.log('guess clicked');
@@ -52,6 +55,8 @@ function game() {
 	});
 
 };
+
+
 
 
 // <input type="text" name="userGuess" id="userGuess" class="text" maxlength="3" autocomplete="off" placeholder="Enter your Guess" required="">
@@ -87,11 +92,21 @@ function checkGuess (guess) {
 // 	// console.log(randomnumber);
 // };
 
+
+
 function checkGuess (secretNumber) {
 
 	// grab user input from box, compare to createdNumber
 	var guess = document.getElementById('userGuess').value;
+	// user input validation for numbers only
+	// input validation for numbers BETWEEN 1-100
+
+
 	var delta = Math.abs(guess - secretNumber);
+	// store guesses in an array.
+	var listOfGuesses = [];
+	listOfGuesses.push(guess);
+
 
 	if (secretNumber = guess) {
 		console.log('success');
@@ -100,8 +115,26 @@ function checkGuess (secretNumber) {
 	else if (delta < 10) {
 		console.log('red hot');
 	}
-	else if (10 < delta < 20 ) {
-		console.log('hot')
+	else if (delta > 10 ) && (delta < 20) {
+		console.log('hot');
+	}
+	else if (delta > 20 ) && (delta < 30) {
+		console.log('warm');
+	}
+	else if (delta > 30 ) && (delta < 40) {
+		console.log('room temperature')
+	}
+	else if (delta > 40 ) && (delta < 50) {
+		console.log('cool');
+	}
+	else if (delta > 50 ) && (delta < 60) {
+		console.log('cold');
+	}
+	else if (delta > 60 ) && (delta < 70) {
+		console.log('frozen');
+	}
+	else if (delta > 70 ) && (delta < 80) {
+		console.log('Ice Planet Hoth');
 	}
 
 
