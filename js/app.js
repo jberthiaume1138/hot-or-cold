a5d01fa947b3808d7bccf028a4c7e877687cffa4
Joseph Berthiaume <joenyc23@gmail.com>
2016-04-09 02:06:09 -0400

    remove some debugging consolelogs

--- js/app.js
+++ js/app.js
@@ -30,23 +30,18 @@ function newGame() {
 };
 
 // --------- event handler for the NEW button ------------
-// could simply do this with jQuery like the code from the repo did with .what
+// could simply do this with jQuery like the code from the repo did with .what ....... but....
 // TODO: figure out a more elegant way to do this with vanilla Javascript
 var listOfElements = document.getElementsByClassName('new');
 var newButton = listOfElements[0];
 newButton.addEventListener("click",function(event) {
-	// handler for li a class=new
-	console.log('new clicked');
-	newGame();
+	newGame();	// handler for li a class=new
 });
 
 // ---------- event handler for button id = guessButton -----------------
 document.getElementById('guessButton').addEventListener("click",function(event) {
 	event.preventDefault();		// do I need this because the html element or type = submit?
-	console.log('guess clicked');
-
 	var guess = document.getElementById('userGuess').value;
-	console.log('you guessed ' + guess);
 
 	// if (validateGuess(guess) == true ) {	// validate the guess
 	// 	console.log('passes validation');
@@ -87,51 +82,41 @@ function compareGuess (guess) {
 	document.getElementById('guessList').innerHTML += '<li>' + guess + '</li>';
 	document.getElementById('feedback').innerHTML = '';
 
-	listOfGuesses.push(guess);
-	document.getElementById('count').innerHTML = listOfGuesses.length;
+	listOfGuesses.push(guess); // add the guess to the array of guesses
+	document.getElementById('count').innerHTML = listOfGuesses.length;	// indicate how many guesses so far
 
 	var delta = Math.abs(guess - secretNumber);
 
 	if (secretNumber == guess) {
-		console.log('success');
 		success();
 	}
 	else if (delta < 10) {
-		console.log('red hot');
 		document.getElementById('feedback').innerHTML = 'Red Hot';
 	}
 	else if ((delta > 10 ) && (delta < 20)) {
-		console.log('hot');
 		document.getElementById('feedback').innerHTML = 'Hot';
 	}
 	else if ((delta > 20 ) && (delta < 30)) {
-		console.log('warm');
 		document.getElementById('feedback').innerHTML = 'Warm';
 	}
 	else if ((delta > 30 ) && (delta < 40)) {
-		console.log('room temperature');
 		document.getElementById('feedback').innerHTML = 'Room Temperature';
 	}
 	else if ((delta > 40 ) && (delta < 50)) {
-		console.log('cool');
 		document.getElementById('feedback').innerHTML = 'Cool';
 	}
 	else if ((delta > 50 ) && (delta < 60)) {
-		console.log('cold');
 		document.getElementById('feedback').innerHTML = 'Cold';
 	}
 	else if ((delta > 60 ) && (delta < 70)) {
-		console.log('frozen');
 		document.getElementById('feedback').innerHTML = 'Frozen';
 	}
 	else {
-		console.log('Ice Planet Hoth');
 		document.getElementById('feedback').innerHTML = 'Ice Planet Hoth';
 	}
 };
 
 function success() {
-	console.log('you win');
 	document.getElementById('feedback').innerHTML = 'Correct!!! You Win!!!';
 	document.getElementById('guessList').innerHTML += '<br/>' + listOfGuesses.length + ' total guesses';
 };
